e159ea5d9baeb4c513b98fa47416fb8e5a06d282
Samiul Amin <samiulamin@hotmail.co.uk>
2026-05-27 13:20:16 +0100

    fet: replace custom auth with django-allauth supporting email verification and Google OAuth

--- web/accounts/tests/test_views.py
+++ web/accounts/tests/test_views.py
@@ -8,47 +8,12 @@ User = get_user_model()
 pytestmark = pytest.mark.django_db
 
 
-def test_get_login_page(client):
-    url = reverse("login")
-    res = client.get(url)
-
-    assert res.status_code == 200
-    assert "Sign in" in res.content.decode()
-
-
-def test_valid_login_redirects(client, user):
-    url = reverse("login")
-    res = client.post(url, {"username": user.username, "password": "12345"})
-
-    assert res.status_code == 302
-    assert res.url == reverse("learning:dashboard")
-
-
-def test_invalid_login(client, user):
-    url = reverse("login")
-    res = client.post(
-        url,
-        {"username": user.username, "password": "wrong"},
-    )
-
-    assert res.status_code == 302
-    assert res.url == reverse("login")
-
-
-def test_logout_redirects_to_login(client_logged_in):
-    url = reverse("logout")
-    res = client_logged_in.post(url)
-
-    assert res.status_code == 302
-    assert res.url == reverse("login")
-
-
 def test_settings_view_requires_login(client):
     url = reverse("settings")
     res = client.get(url)
 
-    assert res.status_code == 302  # Redirect to login
-    assert reverse("login") in res.url
+    assert res.status_code == 302
+    assert reverse("account_login") in res.url
 
 
 def test_settings_view_logged_in(client_logged_in):
